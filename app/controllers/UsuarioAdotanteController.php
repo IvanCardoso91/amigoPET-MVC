1a4b0044e56fac9270661c4df1f10ac29916214c
IvanCardoso91 <ivancardoso1991@gmail.com>
2024-11-27 16:13:21 -0300

    log erro

--- app/controllers/UsuarioAdotanteController.php
+++ app/controllers/UsuarioAdotanteController.php
@@ -1,6 +1,9 @@
 <?php
 ob_start();
 session_start();
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 require_once __DIR__ . '/../models/UsuarioAdotante.php';
 require_once __DIR__ . '/../../config/database.php';
 require '../../vendor/autoload.php';
